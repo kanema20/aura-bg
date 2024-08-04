6f89f8facc7052c83fee94204062d2e2a1fba7a8
Kane Ma <kanema20@gmail.com>
2024-08-04 19:46:03 -0400

    try next app routing

---
+++ src/app/upload/route.js
@@ -0,0 +1,76 @@
+/* eslint-disable semi */
+export const dynamic = "force-dynamic"; // static by default, unless reading the request
+// const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const next = require("next");
+
+const { removeBackground } = require("@imgly/background-removal-node");
+const { v4: guid } = require("uuid");
+// const cors = require("cors");
+
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+
+async function removeImageBackground(imgSource) {
+  const blob = await removeBackground(imgSource, { debug: true });
+  const buffer = Buffer.from(await blob.arrayBuffer());
+  return buffer;
+}
+
+// Set up storage engine with multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log("file ", file);
+    console.log("req ", req);
+    const uploadDir = path.resolve(__dirname, "public/uploads");
+    console.log("uploadDir ", uploadDir);
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${guid()}___${file.originalname}`);
+  }
+});
+const upload = multer({ storage });
+
+const uiAddress =  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://meme-gen-v2.vercel.app";
+
+export default async function handler(req, res) {
+  upload.single("image")
+  try {
+    console.log("req.file ", req.file);
+    if (!req.file) {
+      return res.status(400).send("No file uploaded");
+    }
+    const origFileName = req.file.filename;
+    const uploadedFileDest = `${req.file.destination}/${req.file.filename}`;
+    console.log("uploadedFileDest ", uploadedFileDest);
+    const uploadedFile = fs.readFileSync(uploadedFileDest, "binary");
+    const folderPath = uploadedFileDest.split("aura-bg\\")[1] || uploadedFileDest.split("aura-bg/")[1];
+    console.log("folderPath ", folderPath);
+    const result = await removeImageBackground(folderPath)
+      .catch(error => console.error("Error removing background:", error));
+    console.log("result ", result);
+    const fileName = `${guid()}___${req.file.originalname}`;
+    console.log("fileName ", fileName);
+    const newPath = path.resolve(__dirname, `public/uploads/${fileName}`);
+    console.log("newPath ", newPath);
+    fs.writeFileSync(newPath, result);
+    res.json({
+      data: {
+        message: "File uploaded successfully",
+        imageUrl: fileName,
+        origFileName
+      }
+    });
+  }
+  catch (err) {
+    console.error("error", "server", err);
+    return res.status(500).send("Internal server error");
+  }
+}
